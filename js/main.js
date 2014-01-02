23c999bb51fc7b89b7b7c5dc4a9c51d43feb8349
Gecko <Gecko@Gecko-PC>
2014-01-02 16:50:55 -0300

    scroll to and css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function()
+{
+	$('.menu a').click(function(e)
+	{
+		e.preventDefault();
+		
+		sectionSelector = $(this).data('section');
+		$(window).scrollTo($(sectionSelector), 500);
+	});
+});
