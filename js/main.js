cef23f0a8609ff60364a295d858cfb081d476931
Gecko <songecko@gmail.com>
2014-01-07 23:15:49 -0300

    contact form

--- js/main.js
+++ js/main.js
@@ -7,4 +7,34 @@ $(document).ready(function()
 		sectionSelector = $(this).data('section');
 		$(window).scrollTo($(sectionSelector), 500);
 	});
+	
+	$('.categories a').click(function(e)
+	{
+		e.preventDefault();
+	});
+	
+	$("section#contact form").validate(
+	{
+		onkeyup: false,
+		onclick: false,
+		onfocusout: false,
+		showErrors: function(errorMap, errorList)
+		{
+			$("p.success").hide();
+			$("p.error").show();
+		},
+		submitHandler: function(form)
+		{
+			$.ajax({
+                type: "POST",
+                url: 'processForm.php',
+                data: $(form).serialize(),
+                success: function(data, textStatus, jqXHR)
+                {
+                	$("p.error").hide();
+                	$("p.success").show();
+                }
+			});
+		}
+	});
 });
---
+++ processForm.php
@@ -0,0 +1,37 @@
+<?php 
+
+header("Content-Type: text/html;charset=utf-8");
+
+$contact = $_POST['contact'];
+$name = $contact['name'];
+$phone = $contact['phone'];
+$email = $contact['email'];
+$message = $contact['message'];
+
+//Envio de email
+$to = "songecko@gmail.com";
+$subject = "[Suxess Clothing] new message";
+$body = ' 
+<html> 
+	<head> 
+		<title>Suxess Clothing - New Message</title> 
+	</head> 
+	<body>  
+		<p><b>Name: </b> '.$name.'</p>
+		<p><b>Phone: </b> '.$phone.'</p> 
+		<p><b>Email: </b> '.$email.'</p> 
+		<p><b>Message: </b> '.$message.'</p>
+	</body> 
+</html>
+'; 
+
+//Envío en formato HTML 
+$headers = "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=utf-8\r\n"; 
+
+//Dirección del remitente 
+$headers .= "From: ".$name." <".$email.">\r\n"; 
+
+$sended = mail($to, $subject, $body, $headers);
+
+echo $sended;
